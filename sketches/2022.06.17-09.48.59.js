59a09ce0d4df4ed6c0cf6460af555b778613cd6f
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-06-17 10:21:11 -0300

    primeiro teste fracassado

--- sketches/2022.06.17-09.48.59.js
+++ sketches/2022.06.17-09.48.59.js
@@ -1,13 +1,30 @@
 const canvasSketch = require('canvas-sketch');
+const { polylinesToSVG } = require('canvas-sketch-util/penplot');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 2048, 2048 ],
+  pixelsPerInch: 300,
+  units: 'cm',
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'red';
     context.fillRect(0, 0, width, height);
+
+    return [
+      // Export PNG as first layer
+      context.canvas,
+      // Export SVG for pen plotter as second layer
+      {
+        data: polylinesToSVG({
+          width,
+          height,
+          units
+        }),
+        extension: '.svg'
+      }
+    ];
   };
 };
 
